87eeade4ed46d8240fadf36f454fa7800780e6cd
Oluwatobi Ogundare <tobi.ogundare@gmail.com>
2021-04-10 18:33:54 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.css';
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { AppProvider } from '../context/App';
 
-export function reportWebVitals(metric: any) {
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
